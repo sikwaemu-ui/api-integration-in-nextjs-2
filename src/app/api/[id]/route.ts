8eb4032a77d87a9867a3b85d2a29e9d56d0d3950
Sik-Wa <sikwaemu@gmail.com>
2026-03-02 14:52:33 +0200

    PUT and DELETE Methods done

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -31,7 +31,7 @@ export async function PATCH(
     await connectDB();
 
     const { id } = await context.params;
-    
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return NextResponse.json(
         { error: "invalid student id" },
@@ -56,6 +56,106 @@ export async function PATCH(
 
     return NextResponse.json(updated);
   } catch (error: any) {
-    return NextResponse.json({error:"failed to update student infor", datails: String(error?.message ?? error)}, {status: 400})
+    return NextResponse.json(
+      {
+        error: "failed to update student infor",
+        datails: String(error?.message ?? error),
+      },
+      { status: 400 },
+    );
   }
+}
+
+export async function PUT(
+  request: Request,
+  context: { params: Promise<{ id: string }> },
+) {
+  try {
+    await connectDB();
+
+    const { id } = await context.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return NextResponse.json(
+        { error: " failed to find student id " },
+        { status: 404 },
+      );
+    }
+
+    const body = await request.json();
+
+    if (
+      !body.name ||
+      !body.surname! ||
+      !body.class ||
+      body.grade === null ||
+      body.studentId === null
+    ) {
+      return NextResponse.json(
+        { error: "missing fields required" },
+        { status: 400 },
+      );
+    }
+
+    const updated = await Student.findByIdAndUpdate(id, body, {
+      new: true,
+      overwrite: true,
+      runValidators: true,
+    });
+
+    if (!updated) {
+      return NextResponse.json(
+        { error: " student not found " },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(updated)
+
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "failed to update student " },
+      { status: 404 },
+    );
+  }
+}
+
+
+
+
+
+
+export async function DELETE(request: Request, context:{params: Promise<{id:string}>}){
+try{
+  await connectDB()
+
+const {id} = await context.params 
+
+const body = await request.json()
+
+
+const deleted = await  Student.findByIdAndDelete(id)
+
+if(!deleted){
+  return NextResponse.json({
+    error:"student not found",
+  },{
+    status: 404
+  })
+}
+
+return NextResponse.json(deleted)
+
+
+
+
+
+
+
+
+
+
+
+}catch(error: any){
+return NextResponse.json({error:"failed to delete student"},{status: 404})
+}
 }
